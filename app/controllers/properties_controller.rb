22ec550a94f3a1f37172bd1f6631ae86688b1ae4
Andy Ayers <ayers.andy.m@gmail.com>
2021-10-14 09:58:45 -0500

    Output json to console.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,14 @@
 class PropertiesController < ApplicationController
   def index
     @madison_properties = Property.where(city: 'Madison')
+
+    output_json(@madison_properties)
   end
+
+  private
+
+  def output_json(madison_properties)
+    formatted_madison_properties = madison_properties.map { |property| { property_id: property.rentableId, name: property.name, email: property.email }}
+    puts "\n\n\n" + formatted_madison_properties.inspect + "\n\n\n"
+  end 
 end
