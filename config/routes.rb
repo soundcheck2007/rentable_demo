6dd7088aec7e5b1e944fcf5a8ca567a1fe462e58
Andy Ayers <ayers.andy.m@gmail.com>
2021-10-14 08:47:30 -0500

    change properties path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'properties/index'
+  get 'properties', to: 'properties#index'
   get 'rentable', to: 'rentable#index'
   resources :property, only: [:create]
 end
